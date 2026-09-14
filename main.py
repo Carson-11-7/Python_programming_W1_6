663b127bdbc871fd0a717c280b8298e9c2613db3
Phat Nguyen <tanphat.ng1107@gmail.com>
2026-09-14 10:43:48 +0300

    hi

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ Feed=input("Insert an integer: ")
 Value=int(Feed)
 Remainder=Value%2
 print(f"Value is {Value}")
-print(f"the Remainder is {Remainder} when {Value} is divided by 2.")
+print(f"The remainder is {Remainder} when {Value} is divided by 2.")
